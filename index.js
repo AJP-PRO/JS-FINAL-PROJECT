f65275d089d2ffcabeab40e6feb5e4101802ba39
Austin <austinjpoulsen@gmail.com>
2024-10-03 10:09:39 -0600

    rendering html

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 // API 1: https://www.omdbapi.com/?apikey=a7412df9&s=hobbit
 // API 2: https://omdbapi.com/?apikey=a7412df9&s=lord%20of%20the%20rings
+// const arrayName =[…array1, …array2];
 
+let mediaData = []
 
 async function main() {
     const movies = await fetch("https://www.omdbapi.com/?apikey=a7412df9&s=hobbit")
@@ -9,29 +11,31 @@ async function main() {
     const movieData = await movies.json();
     const movieData2 = await movies2.json();
     const mediaList = document.querySelector(".media");
-    console.log(movieData, movieData2);
+    mediaData =[movieData, movieData2];
+
+    mediaList.innerHTML = mediaData
+    .map(
+        (media) =>
+        `<div class="media">
+            <figure class="poster--wrapper">
+                <img class="poster" src="${media.Poster}" alt="">
+            </figure>
+                <div class="media__title">
+                    ${media.Title}
+                </div>
+                <div class="media__type">
+                    ${media.Type}
+                </div>
+                 <div class="media__year">
+                    ${media.Year}
+                </div>
+        </div>`
+    )
+    .join("");
+
 }
 
 main();
 
 
-/* RENDER HTML */
 
-
-mediaList.innerHTML = movieData + movieData2
-.map((media) => {
- return `<div class="media">
-<figure class="poster--wrapper">
-<img class="poster" src="${media.Poster}" alt="">
-</figure>
-<div class="media__title">
-    ${media.Title}
-</div>
-<div class="media__type">
-    ${media.Type}
-</div>
-<div class="media__year">
-    ${media.Year}
-</div>`})
-
-.join("");
