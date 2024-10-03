5e9732b09ab16228ba24f6df00b5c73abf1ede6b
Austin <austinjpoulsen@gmail.com>
2024-10-03 15:27:02 -0600

    organizing media

--- index.js
+++ index.js
@@ -11,26 +11,32 @@ async function main() {
     const movies2 = await fetch("https://omdbapi.com/?apikey=a7412df9&s=lord%20of%20the%20rings");
     const movieData = await movies.json();
     const movieData2 = await movies2.json();
-    mediaData =[movieData, movieData2];
+
+    mediaData = [...movieData.Search, ...movieData2.Search];
+
     mediaList.innerHTML = mediaData.map((media) => mediaHTML(media)).join("");
 }
 
 main();
 
 
+/* RENDER HTML */
+
    function mediaHTML(media) {
         return `<div class="media">
+        <div class="media__wrapper">
             <figure class="poster--wrapper">
-                <img class="poster" src="${media.Poster}" alt="">
+            <img class="poster" src="${media.Poster}" alt="">
             </figure>
-                <div class="media__title">
-                    ${media.Title}
-                </div>
-                <div class="media__type">
-                    ${media.Type}
-                </div>
-                 <div class="media__year">
-                    ${media.Year}
-                </div>
-        </div>`;
+            <div class="media__title">
+                ${media.Title}
+            </div>
+            <div class="media__type">
+                ${media.Type}
+            </div>
+             <div class="media__year">
+                ${media.Year}
+            </div>
+        </div>
+    </div>`;
     }
