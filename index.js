031fa9ea37f815d41ef08114e51ceb717a767920
Austin <austinjpoulsen@gmail.com>
2024-10-06 17:53:54 -0600

    sort fixed

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 
 // API 1: https://www.omdbapi.com/?apikey=a7412df9&s=hobbit
 // API 2: https://omdbapi.com/?apikey=a7412df9&s=lord%20of%20the%20rings
-// const arrayName =[…array1, …array2];
-
-let mediaData = []
+let mediaData = [];
 const mediaList = document.querySelector(".media");
-
-
-
 async function main() {
-    const movies = await fetch("https://www.omdbapi.com/?apikey=a7412df9&s=hobbit");
-    const movies2 = await fetch("https://omdbapi.com/?apikey=a7412df9&s=lord%20of%20the%20rings");
-    const movieData = await movies.json();
-    const movieData2 = await movies2.json();
-
-    mediaData = [...movieData.Search, ...movieData2.Search];
-
-    mediaList.innerHTML = mediaData.map((media) => mediaHTML(media)).join("");
+  const movies = await fetch(
+    "https://www.omdbapi.com/?apikey=a7412df9&s=hobbit"
+  );
+  const movies2 = await fetch(
+    "https://omdbapi.com/?apikey=a7412df9&s=lord%20of%20the%20rings"
+  );
+  const movieData = await movies.json();
+  const movieData2 = await movies2.json();
+  mediaData = [...movieData.Search, ...movieData2.Search];
+  mediaList.innerHTML = mediaData.map((media) => mediaHTML(media)).join("");
 }
-
 main();
-
-
 /* RENDER HTML */
-
-   function mediaHTML(media) {
-        return `<div class="media">
+function mediaHTML(media) {
+  return `<div class="media">
         <div class="media__wrapper">
             <figure class="poster--wrapper">
             <img class="poster" src="${media.Poster}" alt="">
@@ -41,29 +34,29 @@ main();
             </div>
         </div>
     </div>`;
-    }
-
-
-/* SORTING */
-
-// if (filter === "OLD_TO_NEW") {
-//     media.sort((a, b) => (a.Year - b.Year));
-// } 
-// else if (filter === "NEW_TO_OLD") {
-//     media.sort((a, b) => (b.Year - a.Year));
-// }
-
-// // if filter = games, then return all objects with type "game"
-
-// else if (fiter === "GAMES") {
-//     // return (media.Type = game ? )
-// }
-
-// // if filter = movie, then return all objects with type "movie"
-
-// else if (fiter === "MOVIES") {
-//}
+}
 
 function filterMedia(event) {
-    console.log(event.target.value);
+  const filter = event.target.value;
+  let filteredMedia = [...mediaData]; // Create a copy of the original data
+
+  if (filter === "OLD_TO_NEW") {
+    filteredMedia.sort((a, b) => parseInt(a.Year) - parseInt(b.Year));
+  } else if (filter === "NEW_TO_OLD") {
+    filteredMedia.sort((a, b) => parseInt(b.Year) - parseInt(a.Year));
+  } else if (filter === "GAMES") {
+    filteredMedia = filteredMedia.filter(
+      (media) => media.Type.toLowerCase() === "game"
+    );
+  } else if (filter === "MOVIES") {
+    filteredMedia = filteredMedia.filter(
+      (media) => media.Type.toLowerCase() === "movie"
+    );
+  }
+
+  renderFilteredMedia(filteredMedia);
+}
+
+function renderFilteredMedia(filteredMedia) {
+  mediaList.innerHTML = filteredMedia.map((media) => mediaHTML(media)).join("");
 }
